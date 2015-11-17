0b6b3bd4da0a1544c45d63d0e04e876f1c028f9e
javen123 <thisjaven@gmail.com>
2015-11-17 14:06:38 -0600

    Initial Commit

---
+++ AppsNevaShowcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppsNevaShowcase
+//
+//  Created by Jim Aven on 11/17/15.
+//  Copyright © 2015 Jim Aven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
